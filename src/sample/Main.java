7273a8ba311bc78e2ad6796811b18faf71aaaed5
Konkamon <beebiw2428@gmail.com>
2018-02-25 17:33:10 +0700

    sample

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,17 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage."school Mangement Syeter";
+        primaryStage.setTitle("School Management System");
         primaryStage.setScene(new Scene(root, 600, 390));
         primaryStage.setResizable(false);
         primaryStage.show();
